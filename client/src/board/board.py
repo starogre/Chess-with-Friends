df1148a9d5a53aa2b9752c7392d2a25933449843
CarlP <carlpopper123@gmail.com>
2023-04-22 16:15:42 -0400

    CWF-10-Board class

--- client/src/board/board.py
+++ client/src/board/board.py
@@ -7,20 +7,20 @@ class Board:
         board = [[Square() for _ in range(8)] for _ in range(8)]
         return board
 
-    def get_piece(self, row, col):
+    def get_piece_at(self, row, col):
         return self.board[row][col].get_piece()
 
-    def set_piece(self, row, col, piece):
+    def set_piece_at(self, row, col, piece):
         self.board[row][col].set_piece(piece)
 
     def execute_move(self, start_row, start_col, end_row, end_col):
-        piece = self.get_piece(start_row, start_col)
+        piece = self.get_piece_at(start_row, start_col)
         if piece is None:
             raise ValueError("No piece found at the given start position.")
 
         # If the move is valid, update the board and set the start position to None.
-        self.set_piece(end_row, end_col, piece)
-        self.set_piece(start_row, start_col, None)
+        self.set_piece_at(end_row, end_col, piece)
+        self.set_piece_at(start_row, start_col, None)
 
     def is_empty(self, row, col):
         return self.board[row][col].is_empty()
