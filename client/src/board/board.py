fd183ee6d297d1d998b64c021ce271abb2fa2c56
CarlP <carlpopper123@gmail.com>
2023-04-22 14:51:58 -0400

    CWF-10-Board updating PR

--- client/src/board/board.py
+++ client/src/board/board.py
@@ -3,22 +3,24 @@ class Board:
         self.board = self.initialize_board()
 
     def initialize_board(self):
-        # This can be replaced with actual chess pieces and their positions
-        board = [[None for _ in range(8)] for _ in range(8)]
+        # Initialize the board with empty Square objects
+        board = [[Square() for _ in range(8)] for _ in range(8)]
         return board
 
     def get_piece(self, row, col):
-        return self.board[row][col]
+        return self.board[row][col].get_piece()
 
     def set_piece(self, row, col, piece):
-        self.board[row][col] = piece
+        self.board[row][col].set_piece(piece)
 
     def execute_move(self, start_row, start_col, end_row, end_col):
         piece = self.get_piece(start_row, start_col)
         if piece is None:
             raise ValueError("No piece found at the given start position.")
 
-        # Validate the move before executing it.
-        # If move is valid, update the board and set the start position to None.
+        # If the move is valid, update the board and set the start position to None.
         self.set_piece(end_row, end_col, piece)
         self.set_piece(start_row, start_col, None)
+
+    def is_empty(self, row, col):
+        return self.board[row][col].is_empty()
