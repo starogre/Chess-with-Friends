77bd07b4c35e9d8c377db43346e1a00fe702e368
CarlP <carlpopper123@gmail.com>
2023-04-23 19:32:46 -0400

    CWF-11 updating piece.py

--- client/src/piece/piece.py
+++ client/src/piece/piece.py
@@ -5,11 +5,9 @@ class Piece(ABC):
         self.color = color
         self.position = position
         self.has_moved = False
+    
 
+    @abstractmethod
     def move_piece(self, new_position):
         self.position = new_position
         self.has_moved = True
-
-    @abstractmethod
-    def generate_possible_moves(self):
-        pass
