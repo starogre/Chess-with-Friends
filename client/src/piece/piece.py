abd06e2a0c90f9f2b2d78c2ae1856c77f8a61cba
CarlP <carlpopper123@gmail.com>
2023-04-23 16:46:39 -0400

    CWF-11-PieceClass update

--- client/src/piece/piece.py
+++ client/src/piece/piece.py
@@ -1,7 +1,10 @@
-class Piece:
+from abc import ABC, abstractmethod
+
+class Piece(ABC):
     def __init__(self, color, position):
         self.color = color
         self.position = position
 
+    @abstractmethod
     def generate_possible_moves(self):
-        raise NotImplementedError("This method should be implemented in derived classes.")
+        pass
