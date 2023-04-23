d83280fbc2ae679a1c633a7ed2e98d65b2bfb39a
CarlP <carlpopper123@gmail.com>
2023-04-23 19:22:12 -0400

    CWF-11 updating piece.py

--- client/src/piece/piece.py
+++ client/src/piece/piece.py
@@ -4,6 +4,11 @@ class Piece(ABC):
     def __init__(self, color, position):
         self.color = color
         self.position = position
+        self.has_moved = False
+
+    def move_piece(self, new_position):
+        self.position = new_position
+        self.has_moved = True
 
     @abstractmethod
     def generate_possible_moves(self):
