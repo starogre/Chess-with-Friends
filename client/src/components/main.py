e94511d03a0bd46eb5713529f25d9c47f7780216
JON-DESKTOP\jonsh <jonshoob@gmail.com>
2023-05-18 17:12:39 -0400

    pawn en passant, circular importing fix, pawn diagonal attack, pawn 2 space fix, added comments to main.py testing for pawn

--- client/src/components/main.py
+++ client/src/components/main.py
@@ -13,23 +13,53 @@ pawn_white.position[0] = 3
 pawn_white.position[1] = 2
 pawn_white.has_moved = True
 
-# grab a couple black pawns
+# grab a second white pawn
+pawn_white2 = gc.board.squares[6][6].get_piece()
+
+# grab a couple black pawns to sides of white pawn
 pawn_black_left = gc.board.squares[1][1].get_piece()
 pawn_black_right = gc.board.squares[1][3].get_piece()
 
-# set the left black pawn to the left of the white pawn with a 2 space move
-gc.board.squares[3][1].set_piece(pawn_black_left)
-pawn_black_left.position[0] = 3
+# grab a third black pawn
+pawn_black3 = gc.board.squares[1][5].get_piece()
+# grab a black bishop and move it to block 3rd black pawn
+bishop_black = gc.board.squares[0][6].get_piece()
+gc.board.squares[3][5].set_piece(bishop_black)
+bishop_black.position[0] = 3
+bishop_black.position[1] = 5
+
+# test left pawn en passant
+# gc.board.squares[3][1].set_piece(pawn_black_left)
+# pawn_black_left.position[0] = 3
+# pawn_black_left.position[1] = 1
+# cur_pos_left = pawn_black_left.get_position()
+# StateHandler.last_move = (pawn_black_left, cur_pos_left[0], cur_pos_left[1])
+# last_move = StateHandler.last_move
+# pawn_black_left.moved_two_spaces = True
+
+# test right pawn en passant
+gc.board.squares[3][3].set_piece(pawn_black_right)
+pawn_black_right.position[0] = 3
+pawn_black_right.position[1] = 3
+cur_pos_right = pawn_black_right.get_position()
+StateHandler.last_move = (pawn_black_right, cur_pos_right[0], cur_pos_right[1])
+last_move = StateHandler.last_move
+pawn_black_right.moved_two_spaces = True
+
+# diagonal left attack test white pawn attack left black pawn (don't use at same time as left en passant test)
+gc.board.squares[2][1].set_piece(pawn_black_right)
+pawn_black_left.position[0] = 2
 pawn_black_left.position[1] = 1
 
-cur_pos = pawn_black_left.get_position()
-StateHandler.last_move = (pawn_black_left, cur_pos[0], cur_pos[1])
-last_move = StateHandler.last_move
-
-pawn_black_left.moved_two_spaces = True
-
-print("moved left black pawn and find spaces for white pawn:")
+# print all moves white pawn 1 can make
+print("white pawn #1 can attack a pawn diagonal left, move up one space, or en passant to right:")
 print(pawn_white.find_moves(gc.board, last_move))
 
+# print all moves white pawn 2 can make
+print("white pawn #2 can move up 1 or 2 spaces because it hasn't moved yet:")
+print(pawn_white2.find_moves(gc.board, last_move))
 
+# print all moves black pawn 3 can make
+print("black pawn #3 can move down 1 space because there is a bishop blocking its second space move:")
+print(pawn_black3.find_moves(gc.board, last_move))
 
