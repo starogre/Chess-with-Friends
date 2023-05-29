61471389d1addfa525c910d7fb9bff0882363822
JON-DESKTOP\jonsh <jonshoob@gmail.com>
2023-05-28 23:21:26 -0400

    started rook tests

--- client/unit_tests/piece_test.py
+++ client/unit_tests/piece_test.py
@@ -294,6 +294,96 @@ def test_knight_find_moves_from_corners_and_edges():
     assert knight.find_moves(board) == expected_moves
 
 
+def test_rook_init():
+    piece = Rook("White", (1, 1))
+    assert piece.color == "White"
+    assert piece.position == (1, 1)
+    assert piece.has_moved == False
+
+    piece.move_piece((2, 2))
+    assert piece.position == (2, 2)
+    assert piece.has_moved == True
+
+
+def test_rook_find_moves_empty_board():
+    board = Board()
+
+    for row in board.squares:
+        for square in row:
+            square.set_piece(None)
+
+    rook = Rook("White", (4, 4))
+    expected_moves = [[2, 3], [2, 5], [3, 2], [3, 6], [5, 2], [5, 6], [6, 3], [6, 5]]
+    assert rook.find_moves(board) == expected_moves
+
+
+def test_rook_find_moves_full_board_allied_pieces():
+    board = Board()
+
+    for row in board.squares:
+        for square in row:
+            # all squares occupied by an allied rook
+            square.set_piece(Rook("White", (square.row, square.col)))
+
+    expected_moves = []
+    rook = Rook("White", (4, 4))
+    assert rook.find_moves(board) == expected_moves
+    rook = Rook("White", (0, 0))
+    assert rook.find_moves(board) == expected_moves
+    rook = Rook("White", (8, 8))
+    assert rook.find_moves(board) == expected_moves
+
+
+def test_rook_find_moves_with_enemy_pieces():
+    board = Board()
+
+    for row in board.squares:
+        for square in row:
+            square.set_piece(None)
+
+    # setup enemy pieces on the knights landing squares
+    board.squares[2][3].set_piece(Rook("Black", (2, 3)))
+    board.squares[2][5].set_piece(Rook("Black", (2, 5)))
+    board.squares[3][2].set_piece(Rook("Black", (3, 2)))
+    board.squares[3][6].set_piece(Rook("Black", (3, 6)))
+    board.squares[5][2].set_piece(Rook("Black", (5, 2)))
+    board.squares[5][6].set_piece(Rook("Black", (5, 6)))
+    board.squares[6][3].set_piece(Rook("Black", (6, 3)))
+    board.squares[6][5].set_piece(Rook("Black", (6, 5)))
+
+    rook = Rook("White", (4, 4))
+    expected_moves = [[2, 3], [2, 5], [3, 2], [3, 6], [5, 2], [5, 6], [6, 3], [6, 5]]
+    assert rook.find_moves(board) == expected_moves
+
+
+def test_rook_find_moves_from_corners_and_edges():
+    board = Board()
+
+    for row in board.squares:
+        for square in row:
+            square.set_piece(None)
+
+    rook = Rook("White", (0, 0))
+    expected_moves = [[1, 2], [2, 1]]
+    assert rook.find_moves(board) == expected_moves
+
+    rook = Rook("White", [0, 7])
+    expected_moves = [[1, 5], [2, 6]]
+    assert rook.find_moves(board) == expected_moves
+
+    rook = Rook("White", [7, 0])
+    expected_moves = [[5, 1], [6, 2]]
+    assert rook.find_moves(board) == expected_moves
+
+    rook = Rook("White", [7, 7])
+    expected_moves = [[5, 6], [6, 5]]
+    assert rook.find_moves(board) == expected_moves
+
+    rook = Rook("White", (4, 0))
+    expected_moves = [[2, 1], [3, 2], [5, 2], [6, 1]]
+    assert rook.find_moves(board) == expected_moves
+
+
 def test_king_init():
     piece = King("White", (1, 1))
     assert piece.color == "White"
