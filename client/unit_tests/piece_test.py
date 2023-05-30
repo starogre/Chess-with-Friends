c1ebe8d5f40e93071b6b9860f404a72b45f8c191
JON-DESKTOP\jonsh <jonshoob@gmail.com>
2023-05-30 15:53:49 -0400

    Queen tests passing

--- client/unit_tests/piece_test.py
+++ client/unit_tests/piece_test.py
@@ -498,7 +498,9 @@ def test_queen_find_moves_empty_board():
 
     queen = Queen("White", (4, 4))
     expected_moves = [[5, 5], [6, 6], [7, 7], [3, 5], [2, 6], [1, 7], [5, 3],
-                      [6, 2], [7, 1], [3, 3], [2, 2], [1, 1], [0, 0]]
+                      [6, 2], [7, 1], [3, 3], [2, 2], [1, 1], [0, 0], [5, 4],
+                      [6, 4], [7, 4], [3, 4], [2, 4], [1, 4], [0, 4],
+                      [4, 5], [4, 6], [4, 7], [4, 3], [4, 2], [4, 1], [4, 0]]
     assert queen.find_moves(board) == expected_moves
 
 
@@ -531,9 +533,14 @@ def test_queen_find_moves_with_enemy_pieces():
     board.squares[6][6].set_piece(Queen("Black", (6, 6)))
     board.squares[2][6].set_piece(Queen("Black", (2, 6)))
     board.squares[6][2].set_piece(Queen("Black", (6, 2)))
+    board.squares[2][4].set_piece(Queen("Black", (2, 4)))
+    board.squares[6][4].set_piece(Queen("Black", (6, 4)))
+    board.squares[4][2].set_piece(Queen("Black", (4, 2)))
+    board.squares[4][6].set_piece(Queen("Black", (4, 6)))
 
     queen = Queen("White", (4, 4))
-    expected_moves = [[5, 5], [6, 6], [3, 5], [2, 6], [5, 3], [6, 2], [3, 3], [2, 2]]
+    expected_moves = [[5, 5], [6, 6], [3, 5], [2, 6], [5, 3], [6, 2], [3, 3], [2, 2],
+                      [5, 4], [6, 4], [3, 4], [2, 4], [4, 5], [4, 6], [4, 3], [4, 2]]
     assert queen.find_moves(board) == expected_moves
 
 
@@ -545,27 +552,37 @@ def test_queen_find_moves_from_corners_and_edges():
             square.set_piece(None)
 
     queen = Queen("White", (0, 0))
-    expected_moves = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
+    expected_moves = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+                      [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],
+                      [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
 
     assert queen.find_moves(board) == expected_moves
 
     queen = Queen("White", [0, 7])
-    expected_moves = [[1, 6], [2, 5], [3, 4], [4, 3], [5, 2], [6, 1], [7, 0]]
+    expected_moves = [[1, 6], [2, 5], [3, 4], [4, 3], [5, 2], [6, 1], [7, 0],
+                      [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7],
+                      [0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
 
     assert queen.find_moves(board) == expected_moves
 
     queen = Queen("White", [7, 0])
-    expected_moves = [[6, 1], [5, 2], [4, 3], [3, 4], [2, 5], [1, 6], [0, 7]]
+    expected_moves = [[6, 1], [5, 2], [4, 3], [3, 4], [2, 5], [1, 6], [0, 7],
+                      [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0],
+                      [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7]]
 
     assert queen.find_moves(board) == expected_moves
 
     queen = Queen("White", [7, 7])
-    expected_moves = [[6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1], [0, 0]]
+    expected_moves = [[6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1], [0, 0],
+                      [6, 7], [5, 7], [4, 7], [3, 7], [2, 7], [1, 7], [0, 7],
+                      [7, 6], [7, 5], [7, 4], [7, 3], [7, 2], [7, 1], [7, 0]]
 
     assert queen.find_moves(board) == expected_moves
 
     queen = Queen("White", (4, 0))
-    expected_moves = [[5, 1], [6, 2], [7, 3], [3, 1], [2, 2], [1, 3], [0, 4]]
+    expected_moves = [[5, 1], [6, 2], [7, 3], [3, 1], [2, 2], [1, 3], [0, 4],
+                      [5, 0], [6, 0], [7, 0], [3, 0], [2, 0], [1, 0], [0, 0],
+                      [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7]]
 
     assert queen.find_moves(board) == expected_moves
 
